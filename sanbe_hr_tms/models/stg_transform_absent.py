e16f997116b676cb065c15b51f6152670f1e7a0e
Hanif Wavian P <pratamahanif99@gmail.com>
2024-12-24 09:13:43 +0700

    [FIX] TMS : ubah struktur stg_transform_absent

--- sanbe_hr_tms/models/stg_transform_absent.py
+++ sanbe_hr_tms/models/stg_transform_absent.py
@@ -10,13 +10,18 @@ class StgTransformAbsent (models.Model):
     _name = "stg.transform.absent"
     _description = 'Staging Transform Absent'
 
-    emp_firstname = fields.Char('Employee')
-    emp_code = fields.Char('NIK')
-    dept_code = fields.Char('Department Code')
-    emp_type = fields.Char('Employee Type')
-    emp_group = fields.Char('Employee Group')
-    userid = fields.Char('User ID')
-    dept_name = fields.Char('Department')
-    date = fields.Date('Date')
-    time = fields.Float('Time')
-    in_out = fields.Char('In Out')
+    # emp_firstname = fields.Char('Employee')
+    # emp_code = fields.Char('NIK')
+    # dept_code = fields.Char('Department Code')
+    # emp_type = fields.Char('Employee Type')
+    # emp_group = fields.Char('Employee Group')
+    # userid = fields.Char('User ID')
+    # dept_name = fields.Char('Department')
+    # date = fields.Date('Date')
+    # time = fields.Float('Time')
+    # in_out = fields.Char('In Out')
+
+    userid = fields.Char('Badges Number')
+    logtime = fields.Datetime('Log time')
+    terminal = fields.Char('Terminal')
+    status = fields.Char('Status')
