6c95d50e14de6b445f28c0d8c5b033d0d10a44c8
Bisyri07 <mbisyri33@gmail.com>
2025-02-11 10:45:38 +0700

    [FEAT] TMS: Report Excel, PDF, dan View di Overtime Bundling

--- sanbe_hr_tms/report/__init__.py
+++ sanbe_hr_tms/report/__init__.py
@@ -3,3 +3,5 @@ from . import report_kehadiran_html
 from . import report_empgroup
 from . import report_ot_attendance_html
 from . import report_ot_attendance_excel
+from . import report_ot_bundling_html
+from . import report_ot_bundling_excel
