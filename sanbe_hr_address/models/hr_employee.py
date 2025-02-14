0b9c0f3cf411a9f7b758b8ff13d4c755a2e697a3
Rohim Muhamad <rohimuhamadd@gmail.com>
2025-02-14 22:53:26 +0000

    Revert "Merge [FEAT] PA:Read Only When not Draft"
    This reverts merge request !181

--- sanbe_hr_address/models/hr_employee.py
+++ sanbe_hr_address/models/hr_employee.py
@@ -5,16 +5,3 @@ class HREmployee(models.Model):
     _inherit = "hr.employee"
 
     address_ids = fields.One2many('hr.employee.addresses', 'employee_id', auto_join=True, string='Asset Details')
-    states = fields.Selection([
-        ('draft', "Draft"),
-        ('req_approval', 'Request For Approval'),
-        ('rejected', 'Rejected'),
-        ('approved', 'Approved'),
-        ('inactive', 'Inactive'),
-        ('hold', 'Hold'),
-    ], compute='_get_states'
-    )
-    
-    def _get_states(self):
-        for line in self:
-            return self.env['hr.employee'].browse(line.id).state
