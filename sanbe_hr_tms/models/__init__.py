f1b66510512da671ee95820ee1af042c95203517
Hanif <hanif.wavian@gmail.com>
2025-05-21 14:13:01 +0700

    [FEAT] TMS : Create Tampilan Menu 'Monitoring OT' Pada TMS

--- sanbe_hr_tms/models/__init__.py
+++ sanbe_hr_tms/models/__init__.py
@@ -37,5 +37,6 @@ from . import sb_overtime_attendance
 from . import sb_overtime_bundling
 from . import stg_transform_absent
 from . import sb_leave_allocation
+from . import sb_employee_overtime
 #from . import idris
 
