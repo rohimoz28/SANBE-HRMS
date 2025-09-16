a45a66ed7f74dc602e0fbe9a1030e62510c4ca26
Muhammad Bisyri Mustofa <mbisyri33@gmail.com>
2025-09-16 13:33:46 +0700

    Menghapus file paklaring yang tidak terpakai

--- sanbe_hr_resignation/__manifest__.py
+++ sanbe_hr_resignation/__manifest__.py
@@ -23,7 +23,6 @@
     'data': [
         'security/hr_resignation_security.xml',
         'views/hr_resignation.xml',
-        # 'reports/rpt_paklaring.xml',
         'reports/paklaring_report_tamansari.xml',
         'reports/paklaring_report_cimahi_cimareme.xml',
         'reports/FKPD_report.xml',
