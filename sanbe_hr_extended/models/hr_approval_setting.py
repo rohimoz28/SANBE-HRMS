edf0f0f89729059f8fc1f5dfa0e457c67c3dc2de
Rohim Muhamad <rohimuhamadd@gmail.com>
2025-09-26 22:49:23 +0700

    Create _compute_allowed_employee_ids on hr_approval_setting.py to bypass default record rule

--- sanbe_hr_extended/models/hr_approval_setting.py
+++ sanbe_hr_extended/models/hr_approval_setting.py
@@ -6,26 +6,22 @@ class HrApprovalSetting(models.Model):
 
     branch_id = fields.Many2one('res.branch', string='Business Unit')
     department_id = fields.Many2one('hr.department', domain="[('branch_id','=',branch_id),('is_active','=',True)]", string='Sub Department')
-    approval1_id = fields.Many2one('hr.employee', string='Approval 1', domain=lambda self: self.get_all_employee_domain())
-    approval2_id = fields.Many2one('hr.employee', string='Approval 2')
-    approval3_id = fields.Many2one('hr.employee', string='Approval 3')
-    approval4_id = fields.Many2one('hr.employee', string='Approval 4')
-    approval5_id = fields.Many2one('hr.employee', string='Approval 5')
+    approval1_id = fields.Many2one('hr.employee', string='Approval 1', domain="[('id', 'in', allowed_employee_ids)]")
+    approval2_id = fields.Many2one('hr.employee', string='Approval 2', domain="[('id', 'in', allowed_employee_ids)]")
+    approval3_id = fields.Many2one('hr.employee', string='Approval 3', domain="[('id', 'in', allowed_employee_ids)]")
+    approval4_id = fields.Many2one('hr.employee', string='Approval 4', domain="[('id', 'in', allowed_employee_ids)]")
+    approval5_id = fields.Many2one('hr.employee', string='Approval 5', domain="[('id', 'in', allowed_employee_ids)]")
     desc = fields.Text('Description')
+    allowed_employee_ids = fields.Many2many(
+        'hr.employee',
+        compute='_compute_allowed_employee_ids',
+        store=False, # Tidak disimpan di database
+    )
 
-    @api.model
-    def get_all_employee_domain(self):
-        # bypass branch rule, semua employee kelihatan
-        # emp_ids = self.env['hr.employee'].sudo().search([]).ids
-        # return [('id', 'in', emp_ids)]
-        user = self.env.user
-        import pdb
-        pdb.set_trace()
-        if user.has_group('sanbe_hr_extended.group_hr_approval_setting_staff') \
-           or user.has_group('sanbe_hr_extended.group_hr_approval_setting_supervisor') \
-           or user.has_group('sanbe_hr_extended.group_hr_approval_setting_manager'):
-            # full access (tanpa filter branch)
-            return [('state', '=', 'approved')]
-        else:
-            # tetap ikut branch_id user
-            return [('state', '=', 'approved'),('branch_id', '=', user.branch_id.id)]
+    @api.depends('branch_id')
+    def _compute_allowed_employee_ids(self):
+        all_approved_employees = self.env['hr.employee'].sudo().search([('state', '=', 'approved')])
+        allowed_ids = all_approved_employees.ids
+
+        for record in self:
+            record.allowed_employee_ids = [(6, 0, allowed_ids)]
