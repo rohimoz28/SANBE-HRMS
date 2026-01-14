1e400489cb94a8ed3c5911b5847ee663a8bf292e
Hanif <hanif.wavian@gmail.com>
2026-01-14 15:06:46 +0700

    [2601024] FEAT INSTALLMENT : Buat Modul Baru 'Installment'

---
+++ sanbe_hr_installment/__manifest__.py
@@ -0,0 +1,40 @@
+{
+    'name': "Sanbe HR Installment",
+
+    'summary': "Sanbe HR Installment",
+
+    'description': """
+        Sanbe HR Installment
+    """,
+
+    'author': "Sanbe IT Corp",
+    'website': "http://www.yourcompany.com",
+    "support": "info@sanbe-farma.com",
+    'category': 'Tools',
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'price': 0,
+    'currency': 'USD',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sanbe_hr_tms','hr_payroll_community', 'sanbe_hr_extended'],
+    'data': [
+        # 'views/hr_emp_status.xml',
+        'security/installment_groups.xml',
+        'security/ir.model.access.csv',
+        'security/config_parameter.xml',
+        'security/installment_branch_security.xml',
+        'views/sb_loan_installment.xml',
+        'views/hr_installment_menu.xml'
+    ],
+    'assets': {
+        'web.assets_backend': [
+        ],
+    },
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    "images": ["static/description/banner.png"],
+}
+
