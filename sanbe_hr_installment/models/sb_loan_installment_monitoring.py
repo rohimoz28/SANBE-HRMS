dfdd285c2ea05d57f4329e8c788710d5775ac43c
Rohim Muhamad <rohimuhamadd@gmail.com>
2026-01-21 08:45:59 +0700

    [REF] [2601041] - Split SbLoanInstallmentMonitoring and SbLoanRepayment into separate files.
    For better code maintanability and make more scalable. 1 file many class use in utility file.

--- sanbe_hr_installment/models/sb_loan_installment_monitoring.py
+++ sanbe_hr_installment/models/sb_loan_installment_monitoring.py
@@ -18,15 +18,3 @@ class SbLoanInstallmentMonitoring (models.Model):
     date_from = fields.Date('Start From')
     date_to = fields.Date('To')
     loan_repayment_ids = fields.One2many('sb.loan.repayment', 'loan_monitoring_id', string='Loan Repayment Ids')
-
-class SbLoanRepayment (models.Model):
-    _name = 'sb.loan.repayment'
-    _description = 'Loan Installment Monitoring'
-
-    loan_monitoring_id = fields.Many2one('sb.loan.installment.monitoring', string='Loan Monitoring')
-    pay = fields.Float('Pay')
-    pay_date = fields.Date('Pay Date')
-    bb_amount = fields.Float('BB Amount')
-    pay_amount = fields.Float('Pay Amount')
-    eb_amount = fields.Float('EB Amount')
-    status = fields.Char('Status')
